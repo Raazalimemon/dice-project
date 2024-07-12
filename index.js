fac41128a8aa823af2ffc4963ab8e3473b6eba97
Raazalimemon <raazalimemon123@gmail.com>
2024-07-13 04:07:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var n = Math.random();
 n = n * 6;
 n = Math.floor(n) + 1;
-var randomImageSource1 = "images/dice" + n + ".png";
+var randomImageSource1 = "dice" + n + ".png";
 document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
 var n2 = Math.floor(Math.random() * 6) + 1;
-var randomImageSource2 = "images/dice" + n2 + ".png";
+var randomImageSource2 = "dice" + n2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 if(n > n2){
     document.querySelector("h1").innerHTML = "🚩Raaz wins";
